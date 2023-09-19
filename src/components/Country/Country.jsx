ce91ea2e4a30bfbed1ca823ff499997826f7553d
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-09-19 10:50:37 +0600

    country added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
-    console.log(country)
-    const {name,flags}=country;
+const Country = ({ country, handleVisitedCountry }) => {
+    // console.log(country)
+    const { name, flags, population, area, cca3 } = country;
+
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'purple' : 'green' }}>{name.common}</h3>
+            <div>
+                <h5>Visited countries</h5>
+            </div>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={()=>handleVisitedCountry(country)}>Mark visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
+            {visited ? ' I have visited this country' : ' I want to visit'}
         </div>
-    );
+    )
 };
 
 export default Country;
